90454d5f9e9b19369b2cfba015f2ad134cd1c397
Aravindhan Alagesan <aravindh2090@gmail.com>
2015-03-24 18:35:22 +0530

    Update treeview.js

--- app/models/treeview.js
+++ app/models/treeview.js
@@ -1,7 +1,14 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({	
-	name: DS.attr(),	
-	childTreeViews: DS.hasMany('childtreeview')
+var tenant = DS.Model.extend({
+	name: DS.attr(),
+	childTenants: DS.hasMany('childtenant')
 });
+
+tenant.reopenClass({
+    FIXTURES:[{"childTenants":[{"id":1,"parentId":2,"tenant":{"childTenants":[{"id":5,"parentId":1,"tenant":{"childTenants":[],"id":5,"name":"Sub Sub Tenant3"}},{"id":4,"parentId":1,"tenant":{"childTenants":[],"id":4,"name":"Sub Sub Tenant2"}},{"id":3,"parentId":1,"tenant":{"childTenants":[],"id":3,"name":"Sub Sub Tenant1"}}],"id":1,"name":"Sub Tenant"}}],"id":2,"name":"Aujas"}]
+});
+
+export default tenant;
+
  
